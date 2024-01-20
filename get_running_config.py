d8cea1ac89b5b96b4c895b3f4bc6ea4ed905a68c
choquettejm <choquettejm@outlook.com>
2024-01-20 06:53:42 -0500

    Add files via upload

---
+++ get_running_config.py
@@ -0,0 +1,14 @@
+from ncclient import manager
+import os
+
+#wlcip = os.environ['WLC_IP']
+#Username = os.environ['Username']
+#Pwd = os.environ['Password']
+
+#print(wlcip)
+
+with manager.connect(host="172.25.1.127", port=830, username="admin", hostkey_verify=False, password="!enCgrw1166", device_params={'name':'csr'}) as m:
+
+    c = m.get_config(source='running').data_xml
+
+print(c)
