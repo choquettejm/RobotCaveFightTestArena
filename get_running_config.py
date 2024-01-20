f3aec3e51afaf2bba0cf2268bb1e8643e0786858
choquettejm <choquettejm@outlook.com>
2024-01-20 06:59:26 -0500

    added wlc variable

--- get_running_config.py
+++ get_running_config.py
@@ -1,13 +1,13 @@
 from ncclient import manager
 import os
 
-#wlcip = os.environ['WLC_IP']
-#Username = os.environ['Username']
-#Pwd = os.environ['Password']
+wlcip = os.environ['WLC_IP']
+Username = os.environ['Username']
+Pwd = os.environ['Password']
 
-#print(wlcip)
+print(wlcip)
 
-with manager.connect(host="172.25.1.127", port=830, username="admin", hostkey_verify=False, password="!enCgrw1166", device_params={'name':'csr'}) as m:
+with manager.connect(host=wlcip, port=830, username="admin", hostkey_verify=False, password="!enCgrw1166", device_params={'name':'csr'}) as m:
 
     c = m.get_config(source='running').data_xml
 
